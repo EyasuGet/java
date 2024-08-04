f5f1a4f86fcb50ecae52fc93e96bad68ca500dba
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2024-08-04 19:40:32 +0300

    Add files via upload

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3,7,4,5,9};
+        int low = 0;
+        int high = arr.length-1;
+
+
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+    }
+
+
+
+
+
+
+
+}
