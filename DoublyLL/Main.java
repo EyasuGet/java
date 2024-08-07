4fda3531238ccef30d9be62a6b80cca52fb72348
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2024-08-07 19:19:06 +0300

    Add files via upload

---
+++ DoublyLL/Main.java
@@ -0,0 +1,16 @@
+package DoublyLL;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        DoubleLL list = new DoubleLL();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.display();
+    }
+
+}
